8b847c14d9979f45a47910c93411361f2f1aeee0
Erin Capuchino <valleerin4@gmail.com>
2023-07-31 12:55:51 +0800

    crew desktop:update

--- src/app/crew/page.js
+++ src/app/crew/page.js
@@ -7,18 +7,23 @@ function crew() {
         sm:bg-[url('/crew/background-crew-tablet.jpg')]
         lg:bg-[url('/crew/background-crew-desktop.jpg')]
         bg-cover bg-no-repeat text-center
-        md:text-left md:px-[24px] w-full h-full">
+        md:text-left md:px-[24px] w-full h-full
+        lg:pl-[166px] lg:pr-[137px]
+        ">
         <p className='font-barlow-condensed tracking-[2.7px] md:pl-[14px]'><span className='pr-[18px] font-bold opacity-[0.25]'>02</span> MEET YOUR CREW</p>
         <div className="flex flex-col justify-center 
-        items-center md:flex-col-reverse">
-          <div className="border-b border-[#313b77] flex justify-center md:align-bottom
-             w-[327px] md:border-none mt-[32px] mb-[75px] md:mb-[0px]">
+        items-center md:flex-col-reverse 
+        lg:flex-row-reverse lg:justify-left lg:gap-[81px]">
+          <div className="border-b border-[#313b77]
+            flex justify-center md:align-bottom
+            sm:w-[327px] md:border-none mt-[32px] 
+            mb-[75px] md:mb-[0px] lg:w-auto">
             <div className=' relative  w-[177px] md:w-[456px]
              h-[222px] md:h-[572px]'>
               <Image src='/crew/image-douglas-hurley.png' fill />
             </div>
           </div>
-          <div className='text-center pb-[32px] mx-[24px] '>
+          <div className='text-center pb-[32px] mx-[24px] lg:text-left lg:mx-0'>
             <p className='font-belfair opacity-[0.5] md:text-[24px]'>COMMANDER</p>
             <h1 className='font-belfair text-[24px] md:text-[40px] mt-[8px] mb-[16px]'>DOUGLAS HURLEY</h1>
             <p className='text-[15px] md:text-[16px] max-w-[458px]'>Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.</p>
